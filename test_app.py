1ae0fc653b8239952a832bd76aad4111b6b98c79
geniass1 <amingoma2003@gmail.com>
2020-12-07 19:20:08 +0300

    add test

---
+++ test_app.py
@@ -0,0 +1,13 @@
+from app import app
+import unittest
+class TestViews(unittest.TestCase):
+
+
+    def test_index(self):
+        tester = app.test_client(self)
+        response = tester.get('/')
+        statuscode = response.status_code
+        assert(statuscode, 200)
+
+if __name__ == '__main__':
+    unittest.main() 
