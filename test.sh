a96b3e6fb5baa6e9b7cc0a89df31d8b2d4ac7fe6
geniass1 <amingoma2003@gmail.com>
2020-12-08 19:51:23 +0300

    test.sh was created and dockerfile was updated

---
+++ test.sh
@@ -0,0 +1,4 @@
+echo "Hello, world!"
+set -e
+python3 -m flake8
+python3 test_app.py
--- test_app.py
+++ test_app.py
@@ -1,13 +1,14 @@
 from app import app
 import unittest
+
+
 class TestViews(unittest.TestCase):
-
-
     def test_index(self):
         tester = app.test_client(self)
         response = tester.get('/')
         statuscode = response.status_code
-        assert(statuscode, 200)
+        assert statuscode == 200
+
 
 if __name__ == '__main__':
-    unittest.main() 
+    unittest.main()
