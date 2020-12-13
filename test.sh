0311f5fa8e1f2638ae3eca63ba5bccebe8e5b52c
geniass1 <amingoma2003@gmail.com>
2020-12-13 16:02:02 +0300

    test was completed

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 echo "Hello, world!"
 set -e
 python3 -m flake8
-python3 -m coverage run test_app.py
-python3 -m coverage report
+python3 -m coverage run --source=. test_app.py
+python3 -m coverage report --fail-under=100
