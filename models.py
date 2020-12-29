9a88f16fae28d64ed02bc5bf35e0690b5963e772
geniass1 <amingoma2003@gmail.com>
2020-12-29 22:55:16 +0300

    16 add author

---
+++ models.py
@@ -0,0 +1,24 @@
+from flask_login import UserMixin
+from init import db, manager
+from datetime import datetime
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    login = db.Column(db.String(30), nullable=False)
+    password = db.Column(db.String(300), nullable=False)
+    art_ship = db.relationship('Article', backref='user')
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+@manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
