11cf9d995e440befbcd736e24bbdf4f1effc194c
geniass1 <amingoma2003@gmail.com>
2021-01-01 15:18:05 +0300

    tests was updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # pragma: no cover
-from init import app
-import routes  # noqa: F401
-if __name__ == '__main__':
-    app.run(host="127.0.0.1", port=8080, debug=True)
+from init import app  # pragma: no cover
+import routes  # noqa: F401 # pragma: no cover
+if __name__ == '__main__':  # pragma: no cover
+    app.run(host="127.0.0.1", port=8080, debug=True)  # pragma: no cover
